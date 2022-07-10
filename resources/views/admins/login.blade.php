868a75428750b40b9e81743177477e471ae321b5
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2022-07-10 11:13:50 +0200

    last

--- resources/views/admins/login.blade.php
+++ resources/views/admins/login.blade.php
@@ -1,35 +1,66 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>HTML5 Login Form with validation Example</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-  <link rel="stylesheet" href="{{ asset('public/admin/theme2/css/login.css') }}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('public/admin/dashboard/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{ asset('public/admin/dashboard/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('public/admin/dashboard/dist/css/adminlte.min.css')}}">
 </head>
-<body>
-<!-- partial:index.partial.html -->
-<div id="login-form-wrap">
-  <h2>{{ trans('admin.Login') }} </h2>
-  <form id="login-form" action="" method="post">
-    @csrf
-    <div class="container">
-      @include('admins.alerts.success')
-      @include('admins.alerts.error')
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="index2.html">{{ trans('admin.App Name') }}</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">{{ trans('admin.Login') }}</p>
+
+      <form action="" method="post">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="username" name="username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block form-control"> {{ trans('admin.Sign In') }}</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
     </div>
-    <p>
-    <input type="text" id="username" name="username" placeholder="Username" required><i class="validation"><span></span><span></span></i>
-    </p>
-    <p>
-    <input type="password" id="password" name="password" placeholder="Password" required><i class="validation"><span></span><span></span></i>
-    </p>
-    <p>
-    <input type="submit" id="login" value="Login">
-    </p>
-  </form>
-  <div id="create-account-wrap">
-  </div><!--create-account-wrap-->
-</div><!--login-form-wrap-->
-<!-- partial -->
-  
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{ asset('public/admin/dashboard/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('public/admin/dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('public/admin/dashboard/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
