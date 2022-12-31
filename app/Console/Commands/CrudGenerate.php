0d14af6ddb9491b046de1e4cf4943b14ca6d02fd
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2022-12-31 13:49:00 +0200

    last

--- app/Console/Commands/CrudGenerate.php
+++ app/Console/Commands/CrudGenerate.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 
 class crudGenerate extends Command
 {
@@ -38,16 +39,30 @@ class crudGenerate extends Command
 
         $this->info('make model...');
         $this->info($seperator);
-        Artisan::call('make:model ' . $model);
+        $newfile = fopen(base_path('app/Models/' . $model . '.php'),'w');       //create model
+        $file_content = file_get_contents(base_path('app/CrudGenerate/model.php'));  //take content
+        $content = str_replace('@@table@@', $table ,$file_content);      //handel content
+        $content = str_replace('@@model@@', $model ,$content);      //handel content
+        fwrite($newfile, $content);
+        fclose($newfile); //end
+
 
         $this->info('make controller...');
         $this->info($seperator);
-        Artisan::call('make:controller ' . $model . 'Controller --resource');
+        Artisan::call('make:controller Dashboard/' . $model . 'Controller --resource');
 
         $this->info('make request...');
         $this->info($seperator);
         Artisan::call('make:request ' . $table . '/' . 'CreateRequest');
         Artisan::call('make:request ' . $table . '/' . 'EditRequest');
+
+        $this->info('make views...');
+        File::makeDirectory(base_path('resources/views/' . $table), 0777, true, true);
+        copy(base_path('app/CrudGenerate/CrudViews/index.blade.php'), base_path('resources/views/' . $table . '/index.blade.php'));
+        copy(base_path('app/CrudGenerate/CrudViews/edit.blade.php'), base_path('resources/views/' . $table . '/edit.blade.php'));
+        copy(base_path('app/CrudGenerate/CrudViews/create.blade.php'), base_path('resources/views/' . $table . '/create.blade.php'));
+        copy(base_path('app/CrudGenerate/CrudViews/form.blade.php'), base_path('resources/views/' . $table . '/form.blade.php'));
+
 
         $this->info("crud success.");
     }
