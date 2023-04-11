49c67c535e93004ab39d0f3e5446ee44b4a3919e
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2023-04-11 17:54:31 +0200

    last

--- database/seeders/LaratrustSeeder.php
+++ database/seeders/LaratrustSeeder.php
@@ -15,23 +15,14 @@ class LaratrustSeeder extends Seeder
      */
     public function run()
     {
-        // //role seeder
-        // $role = Role::firstOrCreate([
-        //     'name'          => 'الادارة',
-        //     'display_name'  => 'الادارة',
-        //     'description'   => 'can do any thing',
-        // ]);
-
-        // foreach(config('global.roles') as $key => $values){
-        //     foreach($values as $value){
-        //         $sub_role = Permission::firstOrCreate([
-        //             'name'          => $value . '-' . $key,
-        //             'display_name'  => $value . ' ' . $key,
-        //             'description'   => $value . ' ' . $key,
-        //         ]);
-                
-        //         $role->attachPermissions([$sub_role]);
-        //     }
-        // }
+        foreach(config('global.roles') as $key => $values){
+            foreach($values as $value){
+                $sub_role = Permission::firstOrCreate([
+                    'name'          => $value . '-' . $key,
+                    'display_name'  => $value . ' ' . $key,
+                    'description'   => $value . ' ' . $key,
+                ]);
+            }
+        }
     }
 }
