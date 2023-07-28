9c8f7d04bf061e0b3b993aed4004a463eda29641
ahmedmaher179200 <73036767+ahmedmaher179200@users.noreply.github.com>
2023-07-28 14:29:02 +0300

    last

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
+use App\Notifications\testNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
 
 class HomeController extends Controller
 {
